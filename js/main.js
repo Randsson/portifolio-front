b5ad72af9a0b7e662ec99480f54886a4abf522f7
Randsson <randssondouglas@gmail.com>
2019-11-25 14:38:31 -0300

    add jquery_validate plugin to validate contact form

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
 //Botão do menu
-$('.menu-principal__btn').click(function(){
-    $('.menu-principal').toggleClass('menu-principal--fechado');
+
+$(document).ready(function(){
+    $('.menu-principal__btn').click(function(){
+        $('.menu-principal').toggleClass('menu-principal--fechado');
+    });
+    
+    //Slide depoimentos
+    $('.depoimentos__caixa').slick({
+        autoplay: true,
+        arrows: false,
+        dots: true
+    });
+
+    //validação do formulário de contato
+    $('.formulario').validate({
+        rules: {
+            nome: 'required',
+            email: {
+                required: true,
+                email: true
+            },
+            mensagem: 'required'
+        },
+        messages: {
+            nome: 'O nome não pode estar vaizo.',
+            email: {
+                required: 'Por favor preencha o e-mail.',
+                email: 'Por favor informe um e-mail válido.'
+            },
+            mensagem: 'Mensagem não pode estar vazia.'
+        }
+    });
 });
 
-//Slide depoimentos
-$('.depoimentos__caixa').slick({
-    autoplay: true,
-    arrows: false,
-    dots: true
-});
