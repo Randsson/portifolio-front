031590aa8cd2cb5dbedace0333483fdd7ae02bba
Randsson <randssondouglas@gmail.com>
2019-11-25 11:43:45 -0300

    add slick plugin to build the silde function on depoiments section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+//Botão do menu
 $('.menu-principal__btn').click(function(){
     $('.menu-principal').toggleClass('menu-principal--fechado');
+});
+
+//Slide depoimentos
+$('.depoimentos__caixa').slick({
+    autoplay: true,
+    arrows: false,
+    dots: true
 });
