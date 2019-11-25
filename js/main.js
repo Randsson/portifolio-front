ddbc4284151b204bf84fb7c22a7aecdc7d1b2d7c
Randsson <randssondouglas@gmail.com>
2019-11-25 10:49:15 -0300

    seting jquery function to toggle menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-let botao = document.querySelector('.menu-principal__btn');
-let menuPrincipal = document.querySelector('.menu-principal');
-botao.addEventListener('click', abreFechaMenu);
-
-function abreFechaMenu(evento) {
-    menuPrincipal.classList.toggle('menu-principal--fechado');
-}
+$('.menu-principal__btn').click(function(){
+    $('.menu-principal').toggleClass('menu-principal--fechado');
+});
